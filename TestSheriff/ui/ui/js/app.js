caf0f5990f22a52a5ec5f5fdc16a373a050fc24a
mockersf <mockersf@gmail.com>
2015-01-03 00:12:54 +0100

    last statuses view and filter, infinite scrolling, ...

--- TestSheriff/ui/ui/js/app.js
+++ TestSheriff/ui/ui/js/app.js
@@ -1,24 +1,109 @@
-var host = window.location.href.replace(/\/$/, '');
-host = host.substr(0, host.search('/ui') + 1);
+var url = window.location.href.replace(/\/$/, '');
+
+host = url.substr(0, url.search('/ui') + 1);
 root = 'api/v1/';
 
+nb_item = 15;
 
 (function() {
 
-  var app = angular.module('statuses', []);
+  var app = angular.module('statuses', ['infinite-scroll', 'ui.bootstrap']);
 
-  app.controller('last_statuses', [ '$http', function($http){
+  app.controller('last_statuses', [ '$http', '$modal', '$anchorScroll', function($http, $modal, $anchorScroll){
 
     var self = this;
     this.statuses = [];
+    this.filter = false;
+    this.next_page = "";
+    this.loaded_pages = []
 
     this.get_last = function() {
-      $http.get(host + root + 'statuses')
+      this.filter = false;
+      this.get_last_filtered()
+    };
+
+    this.get_last_filtered = function() {
+      var filter_query = '';
+      for (var key in this.filter) {
+        if (this.filter[key]['enabled']) {
+          if (this.filter[key]['is_details']) {
+            filter_query += '&field=' + key + '&value=' + this.filter[key]['value'];
+          } else {
+            filter_query += '&' + key + '=' + this.filter[key]['value'];
+          }
+        }
+      }
+      this.loaded_pages = [];
+      $anchorScroll();
+      $http.get(host + root + 'statuses?nb_status=' + nb_item + filter_query)
       .success(function(data){
-        self.statuses = data.statuses
+        self.statuses = data.statuses;
+        if (data['pagination'] && data['pagination']['next']) {
+          self.next_page = data['pagination']['next']['href'];
+        } else {
+          self.next_page = "";
+        }
+        self.get_next_page();
+      });
+    };
+
+    this.get_next_page = function() {
+      next_page = this.next_page;
+      if ((next_page != "") && (this.loaded_pages.indexOf(next_page) == -1)) {
+        this.loaded_pages.push(next_page);
+        $http.get(this.next_page)
+        .success(function(data){
+          Array.prototype.push.apply(self.statuses, data.statuses);
+          if (data['pagination'] && data['pagination']['next']) {
+            self.next_page = data['pagination']['next']['href'];
+          } else {
+            self.next_page = "";
+          }
+        });
+      }
+    };
+
+    this.add_filter = function(key, value, is_details) {
+      if (!this.filter)
+        this.filter = {};
+      this.filter[key] = {value: value, is_details: is_details, enabled: true};
+      this.get_last_filtered();
+    };
+
+    this.toggle_filter = function(key) {
+      this.filter[key]['enabled'] = !this.filter[key]['enabled'];
+      this.get_last_filtered()
+    }
+
+    this.add_filter_open = function() {
+      var modalInstance = $modal.open({
+        templateUrl: 'AddFilter.html',
+        controller: 'AddFilter'
+      });
+      modalInstance.result.then(function (filter) {
+        is_details = false;
+        if (['status', 'test_id', 'type'].indexOf(filter['field']) == -1) { is_details = true; }
+        if (filter['field'] != "") {
+          self.add_filter(filter['field'], filter['value'], is_details);
+        }
       });
     };
 
   }])
+
+
+  app.controller('AddFilter', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+
+    $scope.field = "";
+    $scope.value = "";
+
+    $scope.ok = function () {
+      $modalInstance.close({field: $scope.field, value: $scope.value});
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  }]);
 
 })();
